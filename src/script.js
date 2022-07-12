69d7a0841986717dfd7dcc3f42cda894ae992376
Debi7 <debi1@mail.ru>
2022-07-12 22:20:39 +0300

    new

--- src/script.js
+++ src/script.js
@@ -15,31 +15,31 @@ const next = document
   .addEventListener('click', nextSlide);
 
 function nextSlide() {
-  const newSlidesWrapper = Array.from(slidesWrapper.children); 
+  const newSlidesWrapper = Array.from(slidesWrapper.children);
 
-  const slide = newSlidesWrapper.shift(); 
-  slide.classList.add('hidden'); 
-  newSlidesWrapper.push(slide); 
+  const slide = newSlidesWrapper.shift();
+  slide.classList.add('hidden');
+  newSlidesWrapper.push(slide);
 
-  const slideHidden = newSlidesWrapper[2]; 
-  slideHidden.classList.remove('hidden'); 
+  const slideHidden = newSlidesWrapper[2];
+  slideHidden.classList.remove('hidden');
 
-  slidesWrapper.innerHTML = ''; 
-  newSlidesWrapper.forEach((element) => { 
-    slidesWrapper.appendChild(element); 
+  slidesWrapper.innerHTML = '';
+  newSlidesWrapper.forEach((element) => {
+    slidesWrapper.appendChild(element);
   });
 }
 
 
 function prevSlide() {
-  const newSlidesWrapper = Array.from(slidesWrapper.children); 
+  const newSlidesWrapper = Array.from(slidesWrapper.children);
 
-  const prevElem = newSlidesWrapper[2]; 
-  prevElem.classList.add('hidden'); 
+  const prevElem = newSlidesWrapper[2];
+  prevElem.classList.add('hidden');
 
-  const elEnd = newSlidesWrapper.pop(); 
-  elEnd.classList.remove('hidden'); 
-  newSlidesWrapper.unshift(elEnd); 
+  const elEnd = newSlidesWrapper.pop();
+  elEnd.classList.remove('hidden');
+  newSlidesWrapper.unshift(elEnd);
 
   slidesWrapper.innerHTML = '';
   newSlidesWrapper.forEach((element) => {
